391b0f1c4f0fa9232127e70a0c37964940154bad
ElrohirGT <elrohirgt@gmail.com>
2023-08-24 19:23:07 -0600

    fix: Changed the date type

--- Android/app/src/main/java/com/uvg/gt/smartfridgeandroid/Ingredients.kt
+++ Android/app/src/main/java/com/uvg/gt/smartfridgeandroid/Ingredients.kt
@@ -1,7 +1,7 @@
 package com.uvg.gt.smartfridgeandroid
 
-import java.util.Date
+import java.time.LocalDate
 
 data class FoodQuantity(val qualifier: String, val quantity: Float)
-data class Ingredient(val date: Date, val name: String, val category: String, val quantity: FoodQuantity)
+data class Ingredient(val date: LocalDate, val name: String, val category: String, val quantity: FoodQuantity)
 data class IngredientHolder(val category: String, val values: MutableList<Ingredient>)
