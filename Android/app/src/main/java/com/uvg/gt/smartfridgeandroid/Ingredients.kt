49749453f0d804513ca8abb2ea287f68ffd01ffa
Daniel Rayo <danirayoroldan@gmail.com>
2023-09-11 11:52:21 -0600

    Feat: Create Jetpack Compose project for migration

--- Android/app/src/main/java/com/uvg/gt/smartfridgeandroid/Ingredients.kt
+++
@@ -1,7 +0,0 @@
-package com.uvg.gt.smartfridgeandroid
-
-import java.time.LocalDate
-
-data class FoodQuantity(val qualifier: String, val quantity: Float)
-data class Ingredient(val date: LocalDate, val name: String, val category: String, val quantity: FoodQuantity)
-data class IngredientHolder(val category: String, val values: MutableList<Ingredient>)
--- Android/app/src/main/java/com/uvg/gt/smartfridgeandroid/Recipe.kt
+++
@@ -1,3 +0,0 @@
-package com.uvg.gt.smartfridgeandroid
-
-data class Recipe (val id: String, val name: String, val photo: String, val tags: List<String>);
