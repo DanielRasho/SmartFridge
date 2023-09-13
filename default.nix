b78aeec21b3817a3e1e1ad54b73c574daa12b075
ElrohirGT <elrohirgt@gmail.com>
2023-09-12 19:39:59 -0600

    feat: Added nix development deps and rust specification

---
+++ default.nix
@@ -0,0 +1,27 @@
+let
+  rust_overlay = import (builtins.fetchTarball https://github.com/oxalica/rust-overlay/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+in
+  with nixpkgs;
+  stdenv.mkDerivation {
+    name = "rust_overlay_shell";
+    buildInputs = [
+      # to use the latest nightly:
+      (rust-bin.fromRustupToolchainFile ./rust-toolchain)
+      # to use a specific nighly:
+      # (nixpkgs.rustChannelOf { date = "2022-03-29"; channel = "stable"; }).rust
+      # to use the project's rust-toolchain file:
+      # (nixpkgs.rustChannelOf { rustToolchain = ./rust-toolchain; }).rust
+      openssl
+      pkg-config
+	  android-tools
+	  android-studio
+	  android-udev-rules
+	  glibc
+
+    ];
+    shellHook = ''
+      export RUST_BACKTRACE=1
+	  alias and='android-studio > /dev/null 2>&1 &'
+      '';
+  }
