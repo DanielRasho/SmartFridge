af735c36257d5a68c92b512e38610f225e8d0a8d
ElrohirGT <elrohirgt@gmail.com>
2023-09-13 14:21:12 -0600

    feat: Initialized development of the unit selector

--- android/app/src/main/java/uvg/edu/gt/smartfridge/components/unitSelector.kt
+++ android/app/src/main/java/uvg/edu/gt/smartfridge/components/unitSelector.kt
@@ -1,2 +1,54 @@
 package uvg.edu.gt.smartfridge.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import uvg.edu.gt.smartfridge.ui.theme.smartFridgeTheme
+
+@Composable
+fun UnitSelector(
+    onSelected: () -> Unit = {},
+    onCloseButtonClick: () -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = modifier
+                .background(MaterialTheme.colorScheme.inverseSurface)
+                .fillMaxWidth()
+        ) {
+            Text("Unit", color = MaterialTheme.colorScheme.inverseOnSurface, modifier = modifier.padding(16.dp, 0.dp))
+            IconButton(onClick = onCloseButtonClick) {
+                Icon(
+                    Icons.Rounded.Close,
+                    "Close unit selector",
+                    tint = MaterialTheme.colorScheme.inverseOnSurface
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun UnitSelectorPreview() {
+    smartFridgeTheme {
+        UnitSelector()
+    }
+}
