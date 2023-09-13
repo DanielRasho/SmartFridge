8412d17be90a2d9e9e56922493bd7fd2d70a63e2
Daniel Rayo <danirayoroldan@gmail.com>
2023-09-12 22:21:06 -0600

    Feat: Implement app color theme and typography

--- android/app/src/main/java/uvg/edu/gt/smartfridge/MainActivity.kt
+++ android/app/src/main/java/uvg/edu/gt/smartfridge/MainActivity.kt
@@ -3,20 +3,23 @@ package uvg.edu.gt.smartfridge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import uvg.edu.gt.smartfridge.ui.theme.SmartFridgeTheme
+import androidx.compose.ui.unit.sp
+import uvg.edu.gt.smartfridge.ui.theme.smartFridgeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SmartFridgeTheme {
+            smartFridgeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -33,14 +36,16 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier.background(MaterialTheme.colorScheme.primary),
+        color = MaterialTheme.colorScheme.onPrimary,
+        style = MaterialTheme.typography.displayLarge
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    SmartFridgeTheme {
+    smartFridgeTheme {
         Greeting("Android")
     }
 }
--- android/app/src/main/java/uvg/edu/gt/smartfridge/ui/theme/Type.kt
+++ android/app/src/main/java/uvg/edu/gt/smartfridge/ui/theme/Type.kt
@@ -2,33 +2,65 @@ package uvg.edu.gt.smartfridge.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import uvg.edu.gt.smartfridge.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
+// Definition of Outfit font family, the main app font.
+val outfitFamily = FontFamily(
+    Font(R.font.outfit_regular, FontWeight.Normal),
+    Font(R.font.outfit_black, FontWeight.Black),
+    Font(R.font.outfit_bold, FontWeight.Bold),
+    Font(R.font.outfit_extrabold, FontWeight.ExtraBold),
+    Font(R.font.outfit_semibold, FontWeight.SemiBold),
+    Font(R.font.outfit_medium, FontWeight.Medium),
+    Font(R.font.outfit_light, FontWeight.Light),
+    Font(R.font.outfit_extralight, FontWeight.ExtraLight),
+    Font(R.font.outfit_thin, FontWeight.Thin)
+)
+
+// Definition of Typography styles.
+val appTypography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = outfitFamily,
+        fontSize = 44.sp,
+        fontWeight = FontWeight.Black
+    ), //44
+    displayMedium = TextStyle(
+        fontFamily = outfitFamily,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Black
+    ), //24
+    titleMedium = TextStyle(
+        fontFamily = outfitFamily,
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Normal
+    ), // 32
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = outfitFamily,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Normal
+    ), //20
+    bodyMedium = TextStyle(
+        fontFamily = outfitFamily,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
+        fontWeight = FontWeight.Normal
+    ), // 16
+    labelLarge = TextStyle(
+        fontFamily = outfitFamily,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Normal
+    ), // 16
+    labelMedium = TextStyle(
+        fontFamily = outfitFamily,
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Normal
+    ), // 12
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontFamily = outfitFamily,
+        fontSize = 8.sp,
+        fontWeight = FontWeight.Normal
+    ), // 8
 )
