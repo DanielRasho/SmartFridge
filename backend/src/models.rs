192bbcd1239c0b0a143466d140791030ee874818
ElrohirGT <elrohirgt@gmail.com>
2023-10-10 07:13:17 -0600

    fix: renamed field names of models objects

--- backend/src/models.rs
+++ backend/src/models.rs
@@ -13,26 +13,45 @@ pub enum AppThemes {
 /// Represents the settings the user has for the client app.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct UserSettings {
+    #[serde(rename = "Theme")]
     theme: AppThemes,
 }
 
 /// Represents an ingredient that the user needs.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Ingredient {
+    #[serde(rename = "ExpireDate")]
     pub expire_date: DateTime<Utc>,
+
+    #[serde(rename = "Name")]
     pub name: String,
+
+    #[serde(rename = "Category")]
     pub category: String,
+
+    #[serde(rename = "Quantity")]
     pub quantity: u32,
+
+    #[serde(rename = "Unit")]
     pub unit: String,
 }
 
 /// Represents a Food Recipe in the app.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Recipe {
+    #[serde(rename = "Recipe")]
     pub title: String,
+
+    #[serde(rename = "Banner")]
     pub banner: String,
+
+    #[serde(rename = "Tags")]
     pub tags: Vec<String>,
+
+    #[serde(rename = "Ingredients")]
     pub ingredients: Vec<Ingredient>,
+
+    #[serde(rename = "Source")]
     pub source: String,
 }
 
