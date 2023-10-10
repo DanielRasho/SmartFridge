329b387225590154582833e21439432b54b839ec
ElrohirGT <elrohirgt@gmail.com>
2023-10-10 09:28:59 -0600

    feat: Added functions to encrypt password

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,7 +1,9 @@
+use base64::{engine::general_purpose, Engine};
 use hmac::{digest::KeyInit, Hmac};
 use jwt::{SignWithKey, VerifyWithKey};
 use models::JWT_Token;
-use sha2::Sha256;
+use rand::{thread_rng, Rng};
+use sha2::{Digest, Sha256};
 
 mod models;
 mod responses;
@@ -15,6 +17,8 @@ pub enum GenerateJWTErrors {
     ErrorSigningWithKey,
 }
 
+/// Creates a JWT.
+/// This operation may fail as the `token_info` object signing with the key may fail.
 pub fn generate_jwt(secret: &[u8], token_info: JWT_Token) -> Result<String, GenerateJWTErrors> {
     let secret_key: Hmac<Sha256> =
         Hmac::new_from_slice(secret).map_err(|_| GenerateJWTErrors::ErrorGeneratingHmacKey)?;
@@ -29,10 +33,32 @@ pub enum ExtractJWTErrors {
     ErrorExtractingWithKey,
 }
 
+/// Extracts the JWT.
+/// This operation my fail as the JWT may be invalid.
 pub fn extract_jwt(secret: &[u8], token: &str) -> Result<JWT_Token, ExtractJWTErrors> {
     let secret_key: Hmac<Sha256> =
         Hmac::new_from_slice(secret).map_err(|_| ExtractJWTErrors::ErrorGeneratingHmacKey)?;
     token
         .verify_with_key(&secret_key)
         .map_err(|_| ExtractJWTErrors::ErrorExtractingWithKey)
+}
+
+/// Encrypts the given password with a random 16 bytes salt.
+fn encrypt_password(password: String) -> String {
+    let mut rand = thread_rng();
+    let salt: [u8; 16] = rand.gen();
+    encrypt_password_with_salt(password, &salt)
+}
+
+/// Encrypts the password with the given salt.
+fn encrypt_password_with_salt(password: String, salt: &[u8]) -> String {
+    let salt = salt.iter();
+    let password_bytes = password.into_bytes();
+
+    let mut hasher = Sha256::new();
+    hasher.update(password_bytes);
+    let password_bytes = hasher.finalize().to_vec();
+    let password_bytes: Vec<u8> = salt.chain(password_bytes.iter()).copied().collect();
+
+    general_purpose::STANDARD_NO_PAD.encode(password_bytes)
 }
