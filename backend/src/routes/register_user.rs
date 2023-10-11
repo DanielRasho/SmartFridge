a447a8493797d9014d22cafcab5633ef344e8172
ElrohirGT <elrohirgt@gmail.com>
2023-10-11 10:50:20 -0600

    feat: Implemented register_user route with DB

--- backend/src/routes/register_user.rs
+++ backend/src/routes/register_user.rs
@@ -7,13 +7,24 @@ use axum::{response::IntoResponse, Json};
 use hyper::StatusCode;
 use serde::Deserialize;
 use tokio_postgres::Client;
+use uuid::Uuid;
 
-use crate::{encrypt_password, responses::ResponseError};
+use crate::{
+    encrypt_password,
+    models::{AppThemes, UserSettings},
+    responses::ResponseError,
+};
 
 #[derive(Debug)]
 pub enum RegisterUserErrors {
     NoDBConnection,
     InvalidPayload { payload: String },
+    ErrorCheckingIfUserIsAlreadyRegistered,
+    UsernameTaken,
+    ErrorInsertingUserIntoDB,
+    NoUserInserted,
+    ErrorInsertingSettingsIntoDB,
+    NoSettingsInserted,
 }
 
 impl Display for RegisterUserErrors {
@@ -62,13 +73,177 @@ pub async fn register_user(
     };
     tracing::debug!("{} Payload parsed successfully!", tracing_prefix);
 
-    // TODO Check if username exists...
+    tracing::debug!("{} Connecting to DB...", tracing_prefix);
+    let username_exists = match client.as_ref() {
+        Some(conn) => {
+            let result = conn
+                .query_one(
+                    "SELECT user_id FROM sf_user WHERE username=$1",
+                    &[&username],
+                )
+                .await;
+            match result {
+                Ok(r) => !r.is_empty(),
+                Err(err) => {
+                    tracing::error!(
+                        "{} An error `{:?}` occurred while checking if username `{}` exists!",
+                        tracing_prefix,
+                        err,
+                        username
+                    );
+                    let error: ResponseError<_> = (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        RegisterUserErrors::ErrorCheckingIfUserIsAlreadyRegistered,
+                    )
+                        .into();
+                    Err(error)?
+                }
+            }
+        }
+        None => {
+            tracing::debug!(
+                "{} DB Connection not found! Couldn't check if username exists!",
+                tracing_prefix
+            );
+            true
+        }
+    };
+
+    if username_exists {
+        tracing::error!(
+            "{} Username `{}` is already taken!",
+            tracing_prefix,
+            username
+        );
+        let error: ResponseError<_> =
+            (StatusCode::BAD_REQUEST, RegisterUserErrors::UsernameTaken).into();
+        Err(error)?
+    }
 
     tracing::debug!("{} Encrypting password...", tracing_prefix);
     let encrypted = encrypt_password(password);
     tracing::debug!("{} Password encrypted successfully!", tracing_prefix);
 
-    // TODO Insert into DB...
+    tracing::debug!("{} Creating user...", tracing_prefix);
+    let user_id = Uuid::new_v4().to_string();
+
+    match client.as_ref() {
+        Some(conn) => {
+            tracing::debug!("{} DB Connection found!", tracing_prefix);
+
+            tracing::debug!(
+                "{} Inserting user `{}` into DB...",
+                tracing_prefix,
+                username
+            );
+            let result = conn
+                .execute(
+                    "INSERT INTO sf_user VALUES ($1, $2, $3)",
+                    &[&user_id, &username, &encrypted],
+                )
+                .await;
+
+            match result {
+                Ok(rows_modified) => {
+                    if rows_modified > 0 {
+                        tracing::debug!(
+                            "{} User `{}` with ID `{}` inserted into DB!",
+                            tracing_prefix,
+                            username,
+                            user_id
+                        );
+                    } else {
+                        tracing::debug!(
+                            "{} No user inserted into DB! Reason: Unknown",
+                            tracing_prefix
+                        );
+
+                        let error: ResponseError<_> = (
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            RegisterUserErrors::NoUserInserted,
+                        )
+                            .into();
+                        Err(error)?
+                    }
+                }
+                Err(err) => {
+                    tracing::error!(
+                        "{} An error `{:?}` occurred while trying to insert user `{}`!",
+                        tracing_prefix,
+                        err,
+                        username
+                    );
+                    let error: ResponseError<_> = (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        RegisterUserErrors::ErrorInsertingUserIntoDB,
+                    )
+                        .into();
+                    Err(error)?
+                }
+            }
+        }
+        None => {
+            tracing::error!(
+                "{} DB Connection not found!, Couldn't insert user `{}` into DB!",
+                tracing_prefix,
+                username
+            );
+        }
+    }
+
+    tracing::debug!("{} Inserting user settings...", tracing_prefix);
+    match client.as_ref() {
+        Some(conn) => {
+            tracing::debug!("{} DB Connection found!", tracing_prefix);
+            let settings_id = Uuid::new_v4().to_string();
+            let theme = format!("{:?}", AppThemes::default());
+
+            tracing::debug!("{} Inserting settings...", tracing_prefix);
+            match conn
+                .execute(
+                    "INSERT INTO sf_settings VALUES ($1, $2, $3)",
+                    &[&settings_id, &user_id, &theme],
+                )
+                .await
+            {
+                Ok(rows_modified) => {
+                    if rows_modified > 0 {
+                        tracing::debug!(
+                            "{} Settings for user `{}` inserted into DB!",
+                            tracing_prefix,
+                            user_id
+                        );
+                    } else {
+                        tracing::debug!(
+                            "{} No settings inserted into DB! Reason: Unknown",
+                            tracing_prefix
+                        );
+                        let error: ResponseError<_> = (
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            RegisterUserErrors::NoSettingsInserted,
+                        )
+                            .into();
+                        Err(error)?
+                    }
+                }
+                Err(err) => {
+                    tracing::error!("{} An error `{:?}` occurred while inserting settings for user `{}` into DB!", tracing_prefix, err, user_id);
+                    let error: ResponseError<_> = (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        RegisterUserErrors::ErrorInsertingSettingsIntoDB,
+                    )
+                        .into();
+                    Err(error)?
+                }
+            }
+        }
+        None => {
+            tracing::debug!(
+                "{} DB Connection not found!. Couldn't insert user settings into DB!",
+                tracing_prefix
+            );
+        }
+    }
 
     tracing::debug!("{} DONE", tracing_prefix);
     Ok((StatusCode::OK, ""))
