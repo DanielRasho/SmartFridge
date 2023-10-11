5afe5bc20ab12bde200bd98fc5e43062486e189f
ElrohirGT <elrohirgt@gmail.com>
2023-10-11 10:48:57 -0600

    feat: updated the DB

--- db.sql
+++ db.sql
@@ -3,17 +3,17 @@ DROP DATABASE smart_fridge;
 CREATE DATABASE smart_fridge;
 \c smart_fridge;
 
-CREATE TABLE sf_settings (
-	settings_id varchar(64) UNIQUE NOT NULL,
-	theme varchar(64) NOT NULL
-);
-
 CREATE TABLE sf_user (
 	user_id varchar(64) UNIQUE NOT NULL,
 	username varchar(64) NOT NULL,
 	password varchar(64) NOT NULL,
-	settings_id varchar(64) NOT NULL REFERENCES sf_settings(settings_id),
 	PRIMARY KEY( user_id )
+);
+
+CREATE TABLE sf_settings (
+	settings_id varchar(64) UNIQUE NOT NULL,
+	user_id varchar(64) NOT NULL REFERENCES sf_user(user_id),
+	theme varchar(64) NOT NULL
 );
 
 CREATE TABLE sf_session (
