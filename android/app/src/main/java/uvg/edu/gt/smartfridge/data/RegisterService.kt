59fc000c4b82fb8785a5bf5bb4655e2647e25405
DanielRasho <ray22933@uvg.edu.gt>
2023-10-19 20:55:03 -0600

    Feat: First implementation of RegisterService
    for http request required by the Register View.

--- android/app/src/main/java/uvg/edu/gt/smartfridge/data/RegisterService.kt
+++ android/app/src/main/java/uvg/edu/gt/smartfridge/data/RegisterService.kt
@@ -1,12 +1,38 @@
 package uvg.edu.gt.smartfridge.data
 
 import io.ktor.client.HttpClient
+import io.ktor.client.request.headers
+import io.ktor.client.request.request
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
 
 class RegisterService (client: HttpClient) : Service(client) {
 
     suspend fun register(username: String, password: String) : Result<String> {
+        return handleHttpRequest {
 
-        return Result.success("")
+            // Creating Request body
+            val requestBody = buildJsonObject {
+                put("username", username)
+                put("password", password)
+            }.toString()
+
+            // Making Request
+            val response : HttpResponse = client.request(
+                HttpRoutes.BASE_URL + HttpRoutes.LOGIN){
+                method = HttpMethod.Post
+                headers {
+                    append(HttpHeaders.ContentType, "application/json")
+                }
+                setBody( requestBody )
+            }
+
+            "Register Successful"
+        }
     }
 
 }
