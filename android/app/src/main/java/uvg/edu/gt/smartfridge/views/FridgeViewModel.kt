08bbeec5836e8932748aeda03a842f58e1ea7352
DanielRasho <ray22933@uvg.edu.gt>
2023-10-23 11:36:33 -0600

    Fix : Make FridgeViewModel usable.

--- android/app/src/main/java/uvg/edu/gt/smartfridge/views/FridgeViewModel.kt
+++ android/app/src/main/java/uvg/edu/gt/smartfridge/views/FridgeViewModel.kt
@@ -8,6 +8,7 @@ import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.request
 import uvg.edu.gt.smartfridge.data.FridgeService
+import uvg.edu.gt.smartfridge.models.Ingredient
 
 class FridgeViewModel : ViewModel() {
 
@@ -18,7 +19,9 @@ class FridgeViewModel : ViewModel() {
         }
     }
 
-    private val fridgeViewModel = FridgeService(_httpClient)
+    private val fridgeService = FridgeService(_httpClient)
 
-
+    suspend fun getUserIngredients( JWT_TOKEN: String) : Result<List<Ingredient>> {
+        return fridgeService.getIngredients(JWT_TOKEN)
+    }
 }
