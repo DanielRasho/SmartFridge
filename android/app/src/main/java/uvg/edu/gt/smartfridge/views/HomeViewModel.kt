df748bce43648adac28e34a7e4ce3ee22fb600e3
DanielRasho <ray22933@uvg.edu.gt>
2023-10-23 11:35:45 -0600

    Feat: Create Home View Model

---
+++ android/app/src/main/java/uvg/edu/gt/smartfridge/views/HomeViewModel.kt
@@ -0,0 +1,31 @@
+package uvg.edu.gt.smartfridge.views
+
+import androidx.lifecycle.ViewModel
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import uvg.edu.gt.smartfridge.data.HomeService
+import uvg.edu.gt.smartfridge.models.Recipe
+
+class HomeViewModel : ViewModel()  {
+
+    private val _httpClient = HttpClient(CIO) {
+        install (Logging) {
+            logger = Logger.DEFAULT
+            filter { request ->  request.url.host.contains("ktor.io")  }
+        }
+    }
+
+    private val homeService = HomeService(_httpClient)
+
+    suspend fun getRecipesList(JWT_TOKEN : String) : Result<List<Recipe>> {
+        return homeService.getRecipes(JWT_TOKEN)
+    }
+
+    suspend fun searchRecipes(JWT_TOKEN: String, query : String) : Result<List<Recipe>> {
+        return homeService.searchRecipes(JWT_TOKEN, query.trim())
+    }
+
+}
